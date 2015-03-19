64ae75cf693216c3cbbc72f0216a5696abf73a8b
Ross Oldenburg <roldenburg@webcme.net>
2015-03-19 02:42:38 -0500

    added connect/disconnect methods and error notices in command()

--- hardware/DXC390.py
+++ hardware/DXC390.py
@@ -1,21 +1,61 @@
 import serial
+from time import sleep
 
 
 class DXC390(object):
-    def __init__(self, port='/dev/tty.usbserial', iris=0, focus=0, zoom=0):
+    def __init__(self, port='/dev/tty.usbserial-FTXEKIR7D', iris=0, focus=0, zoom=0):
         self._iris = iris
         self._focus = focus
         self._zoom = zoom
         self._port = port
         self._ser = serial.Serial(self._port, timeout=1)
+        self._connected = False
+
+    def connect(self, attempts=3):
+        _attempts = attempts
+        while self._connected == False and attempts > 0:
+            self._ser.flushInput()
+            handshake = self._ser.read(5)
+            if handshake == "OKAY\r":
+                self._ser.flushOutput()
+                self._ser.write("VERS\r")
+                vers = self._ser.read(5)
+                if vers == "V5.2\r":
+                    self._connected = True
+                    return
+            else:
+                self.disconnect()
+                print("Could not connect."),
+                if attempts > 0:
+                    attempts -= 1
+                    print(" -- Trying {} more times.\n".format(attempts))
+                    sleep(2)
+                else:
+                    print("\n")
+
+    def disconnect(self):
+        self._ser.flushOutput()
+        self._ser.write("STOP\r")
+        self._connected = False
+
+    connected = property(lambda self: self._connected)
 
     def command(self, prefix, value, low=0, high=255):
         # TODO: check if prefix is 2 chars and 0<value<255
         xmit = "{prefix}{value:02X}\r".format(prefix=prefix, value=value)
+        self._ser.flushInput()
+        self._ser.flushOutput()
         self._ser.write(xmit)
         recv = self._ser.read(5)
         if recv != xmit:
-            raise Exception("return code ({}) doesn't match command({})".format(recv, xmit))
+            if recv == "CERR\r":
+                print("ERROR: unknown command: {}".format(xmit))
+            elif recv == "MERR\r":
+                print("ERROR: unable to execute: {}".format(xmit))
+            elif recv == "DERR\r":
+                print("ERROR: input out of range: {}".format(xmit))
+            else:
+                print("ERROR: unexpected response: xmit: {} -- recv: {}".format(xmit, recv))
 
     def set_iris(self, value):
         self.command('AE', value)
